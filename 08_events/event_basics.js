d6483192359d9d2b7fec84cc25fe39108ceec03c
Ahmad Ali <ahmad.ali619x@gmail.com>
2025-09-05 17:03:05 +0500

    Learned about events and events propagation

---
+++ 08_events/event_basics.js
@@ -0,0 +1,12 @@
+document.querySelector("#images").addEventListener("click", (e) => {
+    console.log(e.target.tagName);
+    if (e.target.tagName === "IMG") {
+        console.log(e.target.id);
+        let removeIt = e.target.parentNode;
+        removeIt.remove();
+    }
+
+
+    //* 2nd Method
+    // removeIt.parentNode.removeChild(removeIt)
+}, false)
