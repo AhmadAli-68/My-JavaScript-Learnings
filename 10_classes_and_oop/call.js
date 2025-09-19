c42e6a53fa3a04a84e160036a9adead2e6810d29
Ahmad Ali <ahmad.ali619x@gmail.com>
2025-09-19 22:41:21 +0500

    change

--- 10_classes_and_oop/call.js
+++ 10_classes_and_oop/call.js
@@ -6,7 +6,7 @@ function setUsername(username) {
 function createUser(username, email, password) {
     setUsername.call(this, username)
 
-    //? in that above case, humne this ko yaha alone parameter ko use kiya hai, means jb hum this ko alone use krty hain to yeh global objects ko refer kre ga, aur humara global object hi to "setUsername" hai
+    //? in that above case, humne this ko yaha as a alone parameter ko use kiya hai, means jb hum this ko alone use krty hain to yeh global objects ko refer kre ga, aur humara global object hi to "setUsername" hai
     
     // yeh outer function call to ho rha lekin jb yeh call huwa to yeh function call stack se out hogya, means isme jo koi bhi variables thy, woh bhi gaib ho gye. to yeh setUsername humara outer function tha, call stack me yaha tk pohncha hi nai tha.
 
